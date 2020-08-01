22c21bc6fbee1b64efae70029c9fc5affaba9e7f
mas.kinoshita <mas.kinoshita@gmail.com>
2020-08-02 05:59:58 +0900

    Android: add sensor event and method

--- packages/sensors/android/src/main/java/io/flutter/plugins/sensors/SensorsPlugin.java
+++ packages/sensors/android/src/main/java/io/flutter/plugins/sensors/SensorsPlugin.java
@@ -9,10 +9,14 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+
+import androidx.annotation.NonNull;
+
 import io.flutter.plugin.common.EventChannel;
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.PluginRegistry.Registrar;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 /** SensorsPlugin */
@@ -20,7 +24,10 @@ public class SensorsPlugin implements EventChannel.StreamHandler {
   private static final String ACCELEROMETER_CHANNEL_NAME = "plugins.flutter.io/sensors/accelerometer";
   private static final String GYROSCOPE_CHANNEL_NAME = "plugins.flutter.io/sensors/gyroscope";
   private static final String USER_ACCELEROMETER_CHANNEL_NAME = "plugins.flutter.io/sensors/user_accel";
-
+  private static final String ROTATION_VECTOR_CHANNEL_NAME = "plugins.flutter.io/sensors/rotation_vector";
+  private static final String GAME_ROTATION_VECTOR_CHANNEL_NAME = "plugins.flutter.io/sensors/game_rotation_vector";
+  private static final String GEOMAGNETIC_ROTATION_VECTOR_CHANNEL_NAME = "plugins.flutter.io/sensors/geomagnetic_rotation_vector";
+  private static final String MAGNETIC_FIELD_CHANNEL_NAME = "plugins.flutter.io/sensors/magnetic_field";
 
 
   /** Plugin registration. */
@@ -33,6 +40,24 @@ public class SensorsPlugin implements EventChannel.StreamHandler {
 
     final EventChannel gyroscopeChannel = new EventChannel(registrar.messenger(), GYROSCOPE_CHANNEL_NAME);
     gyroscopeChannel.setStreamHandler(new SensorsPlugin(registrar.context(), Sensor.TYPE_GYROSCOPE));
+
+    final EventChannel rotationVectorChannel = new EventChannel(registrar.messenger(), ROTATION_VECTOR_CHANNEL_NAME);
+    rotationVectorChannel.setStreamHandler(new SensorsPlugin(registrar.context(), Sensor.TYPE_ROTATION_VECTOR));
+
+    final EventChannel gameRotationVectorChannel = new EventChannel(registrar.messenger(), GAME_ROTATION_VECTOR_CHANNEL_NAME);
+    gameRotationVectorChannel.setStreamHandler(new SensorsPlugin(registrar.context(), Sensor.TYPE_GAME_ROTATION_VECTOR));
+
+    final EventChannel geomagneticRotationVectorChannel = new EventChannel(registrar.messenger(), GEOMAGNETIC_ROTATION_VECTOR_CHANNEL_NAME);
+    geomagneticRotationVectorChannel.setStreamHandler(new SensorsPlugin(registrar.context(), Sensor.TYPE_GEOMAGNETIC_ROTATION_VECTOR));
+
+    final EventChannel magneticFieldChannel = new EventChannel(registrar.messenger(), MAGNETIC_FIELD_CHANNEL_NAME);
+    magneticFieldChannel.setStreamHandler(new SensorsPlugin(registrar.context(), Sensor.TYPE_MAGNETIC_FIELD));
+
+    final MethodChannel methodGetRotationMatrixChannel = new MethodChannel(registrar.messenger(), MethodGetRotation.CHANNEL);
+    methodGetRotationMatrixChannel.setMethodCallHandler(new MethodGetRotation());
+
+    final MethodChannel methodGetOrientationChannel = new MethodChannel(registrar.messenger(), MethodGetOrientation.CHANNEL);
+    methodGetOrientationChannel.setMethodCallHandler(new MethodGetOrientation());
   }
 
   private SensorEventListener sensorEventListener;
@@ -64,7 +89,7 @@ public class SensorsPlugin implements EventChannel.StreamHandler {
       @Override
       public void onSensorChanged(SensorEvent event) {
         final long timestamp_in_us = event.timestamp/1000;
-        ArrayList<Object> sensorValues = new ArrayList(event.values.length + 1);
+        final ArrayList<Object> sensorValues = new ArrayList(event.values.length + 1);
         sensorValues.add(timestamp_in_us);
         for (double v : event.values) {
           sensorValues.add(v);
@@ -72,5 +97,143 @@ public class SensorsPlugin implements EventChannel.StreamHandler {
         events.success(sensorValues);
       }
     };
+  }
+}
+
+class MethodUtil {
+  public static float[] toFloatArray(List<Double> a) {
+    if(a == null || a.isEmpty()) {
+      return null;
+    }
+    final int size = a.size();
+    final float[] r = new float[size];
+    for(int i=0; i<size; i++) {
+      r[i] = a.get(i).floatValue();
+    }
+    return r;
+  }
+  public static List<Double> toDoubleList(float[] a) {
+    if(a == null || a.length == 0) {
+      return null;
+    }
+    final int size = a.length;
+    final List<Double> r = new ArrayList<>(size);
+    for(float f : a) {
+      r.add(Double.valueOf(f));
+    }
+    return r;
+  }
+}
+
+class MethodGetRotation implements MethodChannel.MethodCallHandler {
+  static final String CHANNEL = "plugins.flutter.io/sensors/get_rotation_matrix";
+  static final String GET_MATRIX = "getRotationMatrix";
+  static final String GET_MATRIX_FROM_VECTOR = "getRotationMatrixFromRotationVector";
+  static final String GET_QUATERNION = "getQuaternion";
+
+  @Override
+  public void onMethodCall(@NonNull MethodCall call, @NonNull MethodChannel.Result result) {
+    if (GET_MATRIX.equals(call.method)) {
+      final List<Double> accel = call.argument("accel");
+      if(accel == null || accel.isEmpty()) {
+        result.error("ErrorAccel", "Please pass acclerometer values", null);
+        return;
+      }
+      final List<Double> mag = call.argument("mag");
+      if(mag == null || mag.isEmpty()) {
+        result.error("ErrorMag", "Please pass magnetrometer values", null);
+        return;
+      }
+      final float[] ret = getRotationMatrix(MethodUtil.toFloatArray(accel), MethodUtil.toFloatArray(mag));
+      if(ret == null) {
+        result.error("Unknown Error", "An error occured in getRotationMatrix", null);
+        return;
+      } else {
+        result.success(MethodUtil.toDoubleList(ret));
+        return;
+      }
+    } else if (GET_MATRIX_FROM_VECTOR.equals(call.method)) {
+      final List<Double> rotationVec = call.argument("rotationVec");
+      if(rotationVec == null || rotationVec.isEmpty()) {
+        result.error("ErrorRotationVec", "Please pass rotation vector values", null);
+        return;
+      }
+      result.success(MethodUtil.toDoubleList(getRotationMatrixFromVector(MethodUtil.toFloatArray(rotationVec))));
+      return;
+    } else if (GET_QUATERNION.equals(call.method)) {
+      final List<Double> rotationVec = call.argument("rotationVec");
+      if(rotationVec == null || rotationVec.isEmpty()) {
+        result.error("ErrorRotationVec", "Please pass rotation vector values", null);
+        return;
+      }
+      result.success(MethodUtil.toDoubleList(getQuaternion(MethodUtil.toFloatArray(rotationVec))));
+      return;
+    } else {
+      result.notImplemented();
+    }
+  }
+
+  static float[] getRotationMatrix(float[] accel, float[] mag) {
+    final float[] rotationMatrix = new float[9];
+    if(SensorManager.getRotationMatrix(rotationMatrix, null, accel, mag)) {
+      return rotationMatrix;
+    } else {
+      return null;
+    }
+  }
+
+  static float[] getRotationMatrixFromVector(float[] rotationVec) {
+    final float[] rotationMatrix = new float[9];
+    SensorManager.getRotationMatrixFromVector(rotationMatrix, rotationVec);
+    return rotationMatrix;
+  }
+
+  static float[] getQuaternion(float[] rotationVec) {
+    final float[] quaternion = new float[4];
+    SensorManager.getQuaternionFromVector(quaternion, rotationVec);
+    return quaternion;
+  }
+}
+
+class MethodGetOrientation implements MethodChannel.MethodCallHandler {
+  static final String CHANNEL = "plugins.flutter.io/sensors/get_orientation";
+  static final String FROM_SENSOR = "getOrientation";
+  static final String FROM_MATRIX = "getOrientationFromRotationMatrix";
+
+  @Override
+  public void onMethodCall(@NonNull MethodCall call, @NonNull MethodChannel.Result result) {
+    if (FROM_SENSOR.equals(call.method)) {
+      final List<Double> accel = call.argument("accel");
+      if(accel == null || accel.isEmpty()) {
+        result.error("ErrorAccel", "Please pass acclerometer values", null);
+        return;
+      }
+      final List<Double> mag = call.argument("mag");
+      if(mag == null || mag.isEmpty()) {
+        result.error("ErrorMag", "Please pass magnetrometer values", null);
+        return;
+      }
+      final float[] rotationMatrix = MethodGetRotation.getRotationMatrix(MethodUtil.toFloatArray((accel)), MethodUtil.toFloatArray(mag));
+      if(rotationMatrix == null) {
+        result.error("Unknown Error", "An error occured in getRotationMatrix", null);
+        return;
+      } else {
+        float[] ret = getOrientationFromMatrix(rotationMatrix);
+        result.success(MethodUtil.toDoubleList(ret));
+        return;
+      }
+    } else if (FROM_MATRIX.equals(call.method)) {
+      final List<Double> rotationMatrix = call.argument("rotationMatrix");
+      float[] ret = getOrientationFromMatrix(MethodUtil.toFloatArray(rotationMatrix));
+      result.success(MethodUtil.toDoubleList(ret));
+    } else {
+      result.notImplemented();
+    }
+  }
+
+  static float[] getOrientationFromMatrix(float[] rotationMatrix) {
+    final float[] orientation = new float[3];
+    SensorManager.getOrientation(rotationMatrix, orientation);
+    return orientation;
   }
 }
--- packages/sensors/lib/sensors.dart
+++ packages/sensors/lib/sensors.dart
@@ -10,9 +10,27 @@ const EventChannel _userAccelerometerEventChannel =
 const EventChannel _gyroscopeEventChannel =
     EventChannel('plugins.flutter.io/sensors/gyroscope');
 
+const EventChannel _rotationVectorEventChannel =
+    EventChannel('plugins.flutter.io/sensors/rotation_vector');
+
+const EventChannel _gameRotationVectorEventChannel =
+    EventChannel('plugins.flutter.io/sensors/game_rotation_vector');
+
+const EventChannel _geomagneticRotationVectorEventChannel =
+    EventChannel('plugins.flutter.io/sensors/geomagnetic_rotation_vector');
+
+const EventChannel _magneticFieldEventChannel =
+    EventChannel('plugins.flutter.io/sensors/magnetic_field');
+
+const MethodChannel _getRotationMethodChannel =
+    MethodChannel('plugins.flutter.io/sensors/get_rotation_matrix');
+
+const MethodChannel _getOrientationMethodChannel =
+    MethodChannel('plugins.flutter.io/sensors/get_orientation');
+
 class AccelerometerEvent {
   AccelerometerEvent(int tus, this.x, this.y, this.z)
-      : t = DateTime.fromMillisecondsSinceEpoch(tus);
+      : t = DateTime.fromMicrosecondsSinceEpoch(tus);
 
   /// creation time of sensor data
   final DateTime t;
@@ -32,7 +50,7 @@ class AccelerometerEvent {
 
 class GyroscopeEvent {
   GyroscopeEvent(int tus, this.x, this.y, this.z)
-      : t = DateTime.fromMillisecondsSinceEpoch(tus);
+      : t = DateTime.fromMicrosecondsSinceEpoch(tus);
 
   /// creation time of sensor data
   final DateTime t;
@@ -52,7 +70,7 @@ class GyroscopeEvent {
 
 class UserAccelerometerEvent {
   UserAccelerometerEvent(int tus, this.x, this.y, this.z)
-      : t = DateTime.fromMillisecondsSinceEpoch(tus);
+      : t = DateTime.fromMicrosecondsSinceEpoch(tus);
 
   /// creation time of sensor data
   final DateTime t;
@@ -70,9 +88,110 @@ class UserAccelerometerEvent {
   String toString() => '[UserAccelerometerEvent (x: $x, y: $y, z: $z)]';
 }
 
+class RotationVectorEvent {
+  RotationVectorEvent(int tus, this.x, this.y, this.z, this.scalar)
+      : t = DateTime.fromMicrosecondsSinceEpoch(tus);
+
+  /// creation time of sensor data
+  final DateTime t;
+
+  /// Rotation vector angle along the x axis
+  final double x;
+
+  /// Rotation vector angle along the y axis
+  final double y;
+
+  /// Rotation vector angle along the z axis
+  final double z;
+
+  /// Scalar of ration vector
+  final double scalar;
+
+  @override
+  String toString() =>
+      '[RotationVectorEvent (x: $x, y: $y, z: $z, scalar: $scalar)]';
+}
+
+class GameRotationVectorEvent {
+  GameRotationVectorEvent(int tus, this.x, this.y, this.z)
+      : t = DateTime.fromMicrosecondsSinceEpoch(tus);
+
+  /// creation time of sensor data
+  final DateTime t;
+
+  /// Game rotation vector angle along the x axis
+  final double x;
+
+  /// Game rotation vector angle along the y axis
+  final double y;
+
+  /// Game rotation vector angle along the z axis
+  final double z;
+
+  @override
+  String toString() => '[GameRotationVectorEvent (x: $x, y: $y, z: $z)]';
+}
+
+class GeomagneticRotationVectorEvent {
+  GeomagneticRotationVectorEvent(int tus, this.x, this.y, this.z)
+      : t = DateTime.fromMicrosecondsSinceEpoch(tus);
+
+  /// creation time of sensor data
+  final DateTime t;
+
+  /// Game rotation vector angle along the x axis
+  final double x;
+
+  /// Game rotation vector angle along the y axis
+  final double y;
+
+  /// Game rotation vector angle along the z axis
+  final double z;
+
+  @override
+  String toString() => '[GeomagneticRotationVectorEvent (x: $x, y: $y, z: $z)]';
+}
+
+class MagneticFieldEvent {
+  MagneticFieldEvent(int tus, this.x, this.y, this.z)
+      : t = DateTime.fromMicrosecondsSinceEpoch(tus);
+
+  /// creation time of sensor data
+  final DateTime t;
+
+  /// Game rotation vector angle along the x axis
+  final double x;
+
+  /// Game rotation vector angle along the y axis
+  final double y;
+
+  /// Game rotation vector angle along the z axis
+  final double z;
+
+  @override
+  String toString() => '[MagneticFieldEvent (x: $x, y: $y, z: $z)]';
+}
+
+class DeviceOrientation {
+  DeviceOrientation(this.azimuth, this.pitch, this.roll);
+
+  /// azimuth angle (rotation angle for z axis)
+  final double azimuth; // z
+
+  /// gradient angle (rotation angle for x axis)
+  final double pitch; // x
+
+  /// rotation angle (rotaiton angle for y axis)
+  final double roll; // y
+}
+
 Stream<AccelerometerEvent> _accelerometerEvents;
 Stream<GyroscopeEvent> _gyroscopeEvents;
 Stream<UserAccelerometerEvent> _userAccelerometerEvents;
+Stream<RotationVectorEvent> _rotationVectorEvents;
+Stream<GameRotationVectorEvent> _gameRotationVectorEvents;
+Stream<GeomagneticRotationVectorEvent> _geomagneticRotationVectorEvents;
+Stream<MagneticFieldEvent> _magneticFieldEvents;
 
 final int _sampleRateDefault = 15;
 int _sampleRate;
@@ -113,4 +232,101 @@ Stream<UserAccelerometerEvent> get userAccelerometerEvents {
             UserAccelerometerEvent(event[0], event[1], event[2], event[3]));
   }
   return _userAccelerometerEvents;
+}
+
+/// Events from the device rotation vector.
+Stream<RotationVectorEvent> get rotationVectorEvents {
+  if (_rotationVectorEvents == null) {
+    _rotationVectorEvents = _rotationVectorEventChannel
+        .receiveBroadcastStream(_sampleRate ?? _sampleRateDefault)
+        .map((dynamic event) => RotationVectorEvent(
+            event[0], event[1], event[2], event[3], event[4]));
+  }
+  return _rotationVectorEvents;
+}
+
+/// Events from the device rotation vector without using magnetic field.
+Stream<GameRotationVectorEvent> get gameRotationVectorEvents {
+  if (_userAccelerometerEvents == null) {
+    _gameRotationVectorEvents = _gameRotationVectorEventChannel
+        .receiveBroadcastStream(_sampleRate ?? _sampleRateDefault)
+        .map((dynamic event) =>
+            GameRotationVectorEvent(event[0], event[1], event[2], event[3]));
+  }
+  return _gameRotationVectorEvents;
+}
+
+/// Events from the device rotation vector with magnetic field.
+Stream<GeomagneticRotationVectorEvent> get geomagneticRotationVectorEvents {
+  if (_geomagneticRotationVectorEvents == null) {
+    _geomagneticRotationVectorEvents = _geomagneticRotationVectorEventChannel
+        .receiveBroadcastStream(_sampleRate ?? _sampleRateDefault)
+        .map((dynamic event) => GeomagneticRotationVectorEvent(
+            event[0], event[1], event[2], event[3]));
+  }
+  return _geomagneticRotationVectorEvents;
+}
+
+/// Events from the magnetic field.
+Stream<MagneticFieldEvent> get magneticFieldEvents {
+  if (_userAccelerometerEvents == null) {
+    _magneticFieldEvents = _magneticFieldEventChannel
+        .receiveBroadcastStream(_sampleRate ?? _sampleRateDefault)
+        .map((dynamic event) =>
+            MagneticFieldEvent(event[0], event[1], event[2], event[3]));
+  }
+  return _magneticFieldEvents;
+}
+
+Future<List<double>> getRotationMatrix(
+    AccelerometerEvent accel, MagneticFieldEvent mag) async {
+  final List<double> accelList = [accel.x, accel.y, accel.z];
+  final List<double> magList = [mag.x, mag.y, mag.z];
+  final List<dynamic> ret = await _getRotationMethodChannel.invokeMethod(
+      'getRotationMatrix',
+      <String, dynamic>{'accel': accelList, 'mag': magList});
+  return ret.cast<double>();
+}
+
+Future<List<double>> getRotationMatrixFromRotationVector(
+    RotationVectorEvent rotationVec) async {
+  final List<double> rotVecList = [
+    rotationVec.x,
+    rotationVec.y,
+    rotationVec.z,
+    rotationVec.scalar
+  ];
+  final List<dynamic> ret = await _getRotationMethodChannel.invokeMethod(
+      'getRotationMatrixFromRotationVector',
+      <String, dynamic>{'rotationVec': rotVecList});
+  return ret.cast<double>();
+}
+
+Future<List<double>> getQuaternion(RotationVectorEvent rotationVec) async {
+  final List<double> rotVecList = [
+    rotationVec.x,
+    rotationVec.y,
+    rotationVec.z,
+    rotationVec.scalar
+  ];
+  final List<dynamic> ret = await _getRotationMethodChannel.invokeMethod(
+      'getQuaternion', <String, dynamic>{'rotationVec': rotVecList});
+  return ret.cast<double>();
+}
+
+Future<DeviceOrientation> getOrientation(
+    AccelerometerEvent accel, MagneticFieldEvent mag) async {
+  final List<double> accelList = [accel.x, accel.y, accel.z];
+  final List<double> magList = [mag.x, mag.y, mag.z];
+  final List<dynamic> list = await _getOrientationMethodChannel.invokeMethod(
+      'getOrientation', <String, dynamic>{'accel': accelList, 'mag': magList});
+  return DeviceOrientation(list[0], list[1], list[2]);
+}
+
+Future<DeviceOrientation> getOrientationFromRotationMatrix(
+    List<double> rotationMatrix) async {
+  final List<dynamic> list = await _getOrientationMethodChannel.invokeMethod(
+      'getOrientationFromRotationMatrix',
+      <String, dynamic>{'rotationMatrix': rotationMatrix});
+  return DeviceOrientation(list[0], list[1], list[2]);
 }
